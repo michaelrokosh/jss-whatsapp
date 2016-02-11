b131705f71e11ec71fab51c44c17dd2d4546f089
Michael Rokosh <mike.rokosh@gmail.com>
2016-02-11 17:39:09 +0200

    :octocat: Initial commit

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  name: 'JSS WhatsApp',
+  description: 'An Android app built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_mdpi': 'resources/icons/mipmap-mdpi/ic_launcher.png',
+  'android_hdpi': 'resources/icons/mipmap-hdpi/ic_launcher.png',
+  'android_xhdpi': 'resources/icons/mipmap-xhdpi/ic_launcher.png'
+});
